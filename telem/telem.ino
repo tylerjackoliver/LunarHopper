15e3a301176d0e2a20deea3eae288734e57d9d73
Shane Lawson <shane-lawson@users.noreply.github.com>
2018-03-10 20:47:05 +0000

    Add code for testing using Receiver

--- telem/telem.ino
+++ telem/telem.ino
@@ -1,6 +1,33 @@
 #include "Telemetry.h"
 
-void setup() {
+// DIRECTIONS: comment/uncomment setup and loop for which Arduino you want to upload to
+
+//********* Receiver; Remote/PC; Currently configured for Remote *********//
+// void setup() {
+//   // put your setup code here, to run once:
+//   Serial.begin(9600);
+//   Serial1.begin(115200, SERIAL_8N2);
+//   Serial.println("Ready for transmission!");
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+//   if (Serial1.available() > 0) {
+//     Serial.print("Coming from HOP(coord): ");
+//     char incomingByte;
+//     while(Serial1.available()>0){
+//     incomingByte = Serial1.read();
+//     Serial.write(incomingByte);
+//     delay(10);
+//     }
+//     //echo back
+//     Serial.print("\r\n");
+//   }
+// }
+
+//********* Transmitter; The Hopper; XBee Coordinator *********//
+void setup()
+{
    //this is where the unicorns and butterflies are born
    Serial.begin(9600);
 }
