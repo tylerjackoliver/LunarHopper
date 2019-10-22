a1c75dd26bbb8a08ef73204d2b646b541a28b863
AlphaProphecy <srowbotham@me.com>
2019-10-22 16:07:23 +0100

    Updated README and added functionality
    Can now read custom files names and the output file is in a more legible format.

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # Lunar Hopper GitHub Repo
 
 This is the repo for the Lunar Hopper on-board computer and control system.
+
+
+## Test script usage
+Tester file format:
+*time (seconds) | command | arguments*
+
+Tester file example:
+'''
+0 start high
+0.01 gyro 1 2
+0.05 accel 1 2 3
+0.1 throttle 50
+0.27 press 90
+1 abort high
+5 disarm high
+10 end
+'''
+
+Test file example output:
+'''
+-0.08030: ACT_PIN_X_POS reads 0
+-0.08025: ACT_PIN_X_NEG reads 0
+-0.08022: ACT_PIN_Y_POS reads 0
+-0.06462: ACT_PIN_Y_NEG reads 0
+-0.06457: SOL_PIN_VENT reads 0
+-0.03106: SOL_PIN_PRES reads 0
+-0.03098: SOL_PIN_OX reads 0
+0.000371: 0 start high
+0.012370: 0.01 gyro 1 2
+0.085079: 0.05 accel 1 2 3
+0.108033: 0.1 throttle 50
+0.326400: 0.27 press 90
+1.030022: 1 abort high
+5.057830: 5 disarm high
+10.06733: 10 end
+10.10730: Test has concluded
+'''
--- Tester.py
+++ Tester.py
@@ -9,9 +9,6 @@ import RPi.GPIO as GPIO
 import time;
 import threading;
 
-GPIO.setwarnings(False);
-
-# All inputs are digital
 class InputThread(object):
     def __init__(self, name, pins, pinNames):
         self.name = name;
@@ -39,6 +36,48 @@ class InputThread(object):
     def getLog(self):
         return self.log;
 
+class Switcher(object):
+    def indirect(self, case, args):
+        method = getattr(self,case);
+        return method(args);
+            
+    def start(self,args):
+        if args[0] == "high":
+            GPIO.output(START_PIN, GPIO.HIGH);
+        else:
+            GPIO.output(START_PIN, GPIO.LOW);
+    
+    def abort(self,args):
+        if args[0] == "high":
+            GPIO.output(ABORT_PIN, GPIO.HIGH);
+        else:
+            GPIO.output(ABORT_PIN, GPIO.LOW);
+    
+    def disarm(self,args):
+        if args[0] == "high":
+            GPIO.output(DISARM_PIN, GPIO.HIGH);
+        else:
+            GPIO.output(DISARM_PIN, GPIO.LOW);
+        
+    def gyro(self,args):
+        gyro("x", args[0]);
+        gyro("y", args[1]);
+    
+    def accel(self,args):
+        accel("x", args[0]);
+        accel("y", args[1]);
+        accel("z", args[2]);
+    
+    def press(self,args):
+        press(args[0]);
+    
+    def throttle(self,args):
+        throttle(args[0]);
+        
+    def end(self,args):
+        for thread in inputControllers:
+            thread.breakLoop();
+
 # Analog Outputs
 PRES_TRANS_PIN = 2;  # Input from Pressure Transducer [ANALOG]
 GYRO_PIN_X = 3;      # x-axis Gyro is connected to analog pin
@@ -63,6 +102,7 @@ SOL_PIN_PRES = 8;    # Output to PRESSURE solenoid control (SV1)
 SOL_PIN_OX = 7;       # Output to OX solenoid control(SV3)
 
 GPIO.setmode(GPIO.BCM);
+GPIO.setwarnings(False);
 
 GPIO.setup(PRES_TRANS_PIN, GPIO.OUT);
 GPIO.setup(GYRO_PIN_X, GPIO.OUT);
@@ -106,14 +146,23 @@ def press(value):
 def throttle(value):
     GPIO.output(THROTTLE_PIN, 0); #ANALOG VALUE to be figured out
 
-def stringTimeMillisSince(start):
-    return str(start - int(round(time.time() * 1000)));
+def stringTimeSince(start,time,sg):
+    return str(time-start)[:sg];
 
 inputControllers = [InputThread("First",[ACT_PIN_X_POS,ACT_PIN_X_NEG,ACT_PIN_Y_POS],["ACT_PIN_X_POS", "ACT_PIN_X_NEG", "ACT_PIN_Y_POS"])];
 inputControllers.append(InputThread("Second",[ACT_PIN_Y_NEG,SOL_PIN_VENT],["ACT_PIN_Y_NEG", "SOL_PIN_VENT"]));
 inputControllers.append(InputThread("Third",[SOL_PIN_PRES,SOL_PIN_OX],["SOL_PIN_PRES", "SOL_PIN_OX"]));
 
+while (True):
+	fileName = input("Enter name of test file: ");
+	try:
+		inFile = open(fileName,"r");
+		break;
+	except:
+		print("Error opening file");
+
 threads = [];
+
 try:
     for inObject in inputControllers:
         print("Creating new thread: "+ inObject.getName());
@@ -125,56 +174,15 @@ try:
 except Exception as e:
     print(e);
     print("Error creating thread");
-        
-class Switcher(object):
-    def indirect(self, case, args):
-        method = getattr(self,case);
-        return method(args);
-            
-    def start(self,args):
-        if args[0] == "high":
-            GPIO.output(START_PIN, GPIO.HIGH);
-        else:
-            GPIO.output(START_PIN, GPIO.LOW);
-    
-    def abort(self,args):
-        if args[0] == "high":
-            GPIO.output(ABORT_PIN, GPIO.HIGH);
-        else:
-            GPIO.output(ABORT_PIN, GPIO.LOW);
-    
-    def disarm(self,args):
-        if args[0] == "high":
-            GPIO.output(DISARM_PIN, GPIO.HIGH);
-        else:
-            GPIO.output(DISARM_PIN, GPIO.LOW);
-        
-    def gyro(self,args):
-        gyro("x", args[0]);
-        gyro("y", args[1]);
-    
-    def accel(self,args):
-        accel("x", args[0]);
-        accel("y", args[1]);
-        accel("z", args[2]);
-    
-    def press(self,args):
-        press(args[0]);
-    
-    def throttle(self,args):
-        throttle(args[0]);
-        
-    def end(self,args):
-        for thread in inputControllers:
-            thread.breakLoop();
-            
-inFile = open("test.txt","r");
+
+
+
 
 log = [];
-
 startTime = time.time();
 switcher = Switcher();
 
+# Main control loop
 for line in inFile.readlines():
     cmd = line.lower().split();
     timeToSleep = float(cmd[0]) + startTime - time.time()
@@ -193,10 +201,15 @@ for thread in inputControllers:
 
 outputLog = sorted(log, key=lambda record: record[0]);
 
-outFile = open(str(time.time())+".txt","w+"); # Result file
+outFile = open(str(time.time())+"_Log.txt","w+"); # Result file
 
 for tuple in outputLog:
-    outFile.write(str(tuple[0]) + ": " + tuple[1] + "\n");
+    if (not "\n" in tuple[1]):
+
+        outTuple = (tuple[0], tuple[1] + "\n");
+    else:
+        outTuple = tuple
+    outFile.write(stringTimeSince(startTime,outTuple[0],8) + ": " + outTuple[1]);
 
 outFile.close();
 
